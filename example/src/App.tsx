35f335a188f43a62906ce4b226f8a5d291b10207
moretall <seongho4996@naver.com>
2022-02-26 13:29:30 +0900

    add example and fix isFile funciton

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useCallback, useEffect, useMemo, useState} from 'react';
 import './App.css';
+import {FileListTree, ReactFileListTree} from "filelist-treeview";
 
 function App() {
+  const [fileList, setFileList] = useState<File[]>([]);
+  const onChangeFile = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files ?? [];
+    setFileList((fileList) => fileList.concat(Array.from(files)));
+  }, []);
+
+  const elements = useMemo(() => {
+      return new ReactFileListTree(new FileListTree(fileList)).getComponent();
+  }, [fileList]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          {elements}
+        <input
+            {...{
+                directory : "",
+                webkitdirectory: "",
+            }}
+               type="file"
+               accept={".zip,.tar"}
+               onChange={onChangeFile}
+        />
+        <input type="file"
+               onChange={onChangeFile}
+        />
+      </div>
   );
 }
 
